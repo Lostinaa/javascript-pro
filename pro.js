76610ceedd6d1267bce18563784357bfb8d94176
lostinaa <docbeatz8@gmail.com>
2023-04-29 23:21:04 +0300

    newest commit

--- pro.js
+++ pro.js
@@ -2,18 +2,17 @@
 //document.getElementById("counter").innerText=33
 
   let countEr = document.getElementById("counter")
-
-  console.log(countEr)
- let count =0
-
- function increment(){
+let count =0
+function increment(){
   count=count+2
   document.getElementById("counter").innerText= count 
+} 
 
-    
+ function save(){
+
+    console.log(count)
+
  } 
-
- 
 
 
  
