039b7a34ca3dbcba092a4cb91f9dc5c61e2a28f9
doc tina <lostinaa@github.com>
2023-05-05 22:24:01 +0300

    para

--- pro.js
+++ pro.js
@@ -1,19 +1,30 @@
 
 //document.getElementById("counter").innerText=33
 
-  let countEr = document.getElementById("counter")
-let count =0
+  //let countEr = document.getElementById("counter")
+/*let count =0
 function increment(){
   count=count+2
   document.getElementById("counter").innerText= count 
 } 
-
+*/
+let greeting=hey
+let name=dopy
+let text;
+let all;
  function save(){
-
-    console.log(count)
+  text=document.getElementById("content").innerText
+    all=text + name
+   
+    console.log(all)
 
  } 
-
+    save()
+function dope(){
+count= + 1 
+document.getElementById("counter").innerText=count
+ 
+}
 
  
 
